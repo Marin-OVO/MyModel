1d99bbb842d735d03a4bbbe70857a230c6f7e8f6
Marin-OVO <1922590765@qq.com>
2026-01-07 19:16:16 +0800

    modify:hfp

--- model/modules/hfp.py
+++ model/modules/hfp.py
@@ -30,24 +30,35 @@ class HighFrequencyGenerator(nn.Module):
 
         return Fi
 
+
 class CP(nn.Module):
-    def __init__(self, in_ch, out_ch):
+    def __init__(self, channels, reduction=16, k=16):
         super().__init__()
-        self.gap = nn.Sequential(
-            nn.AdaptiveAvgPool2d(1),
+
+        self.gap = nn.AdaptiveAvgPool2d((k, k))
+        self.gmp = nn.AdaptiveMaxPool2d((k, k))
+
+        self.conv_gap = nn.Sequential(
             nn.ReLU(inplace=True),
-            nn.Conv2d(in_ch, out_ch, 1, 1)
+            nn.Conv2d(
+                channels,
+                channels // reduction,
+                kernel_size=1,
+                groups=channels // reduction,
+                bias=False
+            )
         )
-        self.gmp = nn.AdaptiveMaxPool2d(1)
 
-class CP(nn.Module):
-    def __init__(self, channels, reduction=16):
-        super().__init__()
-
-        self.gap = nn.AdaptiveAvgPool2d(1)
-        self.gmp = nn.AdaptiveMaxPool2d(1)
-
-        self.conv1 = nn.Conv2d(channels, channels // reduction, 1, bias=False)
+        self.conv_gmp = nn.Sequential(
+            nn.ReLU(inplace=True),
+            nn.Conv2d(
+                channels,
+                channels // reduction,
+                kernel_size=1,
+                groups=channels // reduction,
+                bias=False
+            )
+        )
 
         self.conv_fuse = nn.Conv2d(
             2 * (channels // reduction),
@@ -56,66 +67,92 @@ class CP(nn.Module):
             bias=False
         )
 
-        self.act = nn.Sigmoid()
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, Fi, Ci):
 
         gap = self.conv_gap(self.gap(Fi))
         gmp = self.conv_gmp(self.gmp(Fi))
 
-        # s: pixel-by-pixel summation
-        s = gap + gmp
+        gap = gap.sum(dim=(2, 3), keepdim=True)
+        gmp = gmp.sum(dim=(2, 3), keepdim=True)
 
-        # c: channel-wise concatenation
         c = torch.cat([gap, gmp], dim=1)
+        ucp = self.sigmoid(self.conv_fuse(c))
 
-        # channel weight
-        u_cp = self.act(self.conv_fuse(c))
-
-        # apply to Ci
-        return Ci * u_cp
+        return Ci * ucp
 
 
+class SP(nn.Module):
+    def __init__(self, channels):
+        super().__init__()
+        self.conv = nn.Conv2d(channels, 1, kernel_size=1, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, Fi, Ci):
+        u_sp = self.sigmoid(self.conv(Fi))
+
+        return Ci * u_sp
+
+class HFP(nn.Module):
+    def __init__(self, channels, alpha=0.2):
+        super().__init__()
+
+        self.hfg = HighFrequencyGenerator(alpha)
+        self.cp = CP(channels)
+        self.sp = SP(channels)
+
+        self.conv3 = nn.Conv2d(channels, channels, kernel_size=3, padding=1)
+
+    def forward(self, Ci):
+        Fi = self.hfg(Ci)
+
+        C_cp = self.cp(Fi, Ci)
+        C_sp = self.sp(Fi, Ci)
+
+        C_fuse = C_cp + C_sp
+
+        Ci_hfp = self.conv3(C_fuse)
+
+        return Ci_hfp
 
 
-
-
-if __name__ == "__main__":
-    import torchvision.transforms as T
-    from PIL import Image
-    import matplotlib.pyplot as plt
-
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-
-    img = Image.open("img.png").convert("L")
-    transform = T.ToTensor()
-    x = transform(img).unsqueeze(0).to(device)
-
-    model = HighFrequencyGenerator(
-        alpha=0.1
-    ).to(device)
-
-    with torch.no_grad():
-        y = model(x)
-
-    x_np = x[0, 0].cpu().numpy()
-    y_np = y[0, 0].cpu().numpy()
-
-    plt.figure(figsize=(10, 4))
-    plt.subplot(1, 3, 1)
-    plt.title("Input")
-    plt.imshow(x_np, cmap="gray")
-    plt.axis("off")
-
-    plt.subplot(1, 3, 2)
-    plt.title("Filtered")
-    plt.imshow(y_np, cmap="gray")
-    plt.axis("off")
-
-    plt.subplot(1, 3, 3)
-    plt.title("High-frequency (|Δ|)")
-    plt.imshow(abs(y_np - x_np), cmap="gray")
-    plt.axis("off")
-
-    plt.tight_layout()
-    plt.show()
+# if __name__ == "__main__":
+#     import torchvision.transforms as T
+#     from PIL import Image
+#     import matplotlib.pyplot as plt
+#
+#     device = "cuda" if torch.cuda.is_available() else "cpu"
+#
+#     img = Image.open("0a2e15e29.png").convert("L")
+#     transform = T.ToTensor()
+#     x = transform(img).unsqueeze(0).to(device)
+#
+#     model = HighFrequencyGenerator(
+#         alpha=0.1
+#     ).to(device)
+#
+#     with torch.no_grad():
+#         y = model(x)
+#
+#     x_np = x[0, 0].cpu().numpy()
+#     y_np = y[0, 0].cpu().numpy()
+#
+#     plt.figure(figsize=(10, 4))
+#     plt.subplot(1, 3, 1)
+#     plt.title("Input")
+#     plt.imshow(x_np, cmap="gray")
+#     plt.axis("off")
+#
+#     plt.subplot(1, 3, 2)
+#     plt.title("Filtered")
+#     plt.imshow(y_np, cmap="gray")
+#     plt.axis("off")
+#
+#     plt.subplot(1, 3, 3)
+#     plt.title("High-frequency (|Δ|)")
+#     plt.imshow(abs(y_np - x_np), cmap="gray")
+#     plt.axis("off")
+#
+#     plt.tight_layout()
+#     plt.show()
